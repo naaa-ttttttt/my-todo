9ccffc0497a93e715168d2298e8348d1ca014b11
Nathaniel Parah <nathanielparah579@gmail.com>
2026-09-02 22:52:41 +0100

    feat: add function that gets all task from the database

--- db.py
+++ db.py
@@ -26,5 +26,14 @@ def add_todo(task):
     print("Ran Successful")
 
 
+def get_all_todos():
+    connection = sqlite3.connect("todos.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM todos")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
 if __name__ == "__main__":
     #add_todo("pray and code")
+    print(get_all_todos())
