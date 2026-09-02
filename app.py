d269d3fd9fd14b583e36e29db99a6821ed226cd1
Nathaniel Parah <nathanielparah579@gmail.com>
2026-09-03 00:18:04 +0100

    feat: add GET /todos route to fetch all todos

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def home():
     return "Welcome"
     
 
+@app.route("/todos")
+def todos():
+    rows = get_all_todos()
+    return jsonify(rows)
+
 if __name__ == "__main__":
     app.run(debug=True)
