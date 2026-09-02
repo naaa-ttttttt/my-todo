47c6444829915dbe7a76fba1953bb5bce7d752e0
Nathaniel Parah <nathanielparah579@gmail.com>
2026-09-02 23:40:40 +0100

    feat: add delete function

--- db.py
+++ db.py
@@ -42,16 +42,16 @@ def update_todo(todo_id):
     connection.close()
 
 
-#def delete_todo(todo_id):
- #   connection = sqlite3.connect("todos.db")
-  #  cursor = connection.cursor()
-   # cursor.execute("DELETE FROM todos WHERE id = ?", (todo_id,))
-    #connection.commit()
-    #connection.close()
+def delete_todo(todo_id):
+    connection = sqlite3.connect("todos.db")
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM todos WHERE id = ?", (todo_id,))
+    connection.commit()
+    connection.close()
 
 
 if __name__ == "__main__":
     add_todo("pray and code")
     print(get_all_todos())
     update_todo(1)
-   # delete_todo(1)
+    delete_todo(1)
